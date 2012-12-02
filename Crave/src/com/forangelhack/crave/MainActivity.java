307d164c2e9ef9a803fd30940edcf7277357b4b4
Margaret <margaretmz@gmail.com>
2012-12-01 22:35:01 -0800

    Main screen with action bar

--- Crave/src/com/forangelhack/crave/MainActivity.java
+++ Crave/src/com/forangelhack/crave/MainActivity.java
@@ -3,6 +3,8 @@ package com.forangelhack.crave;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -15,8 +17,30 @@ public class MainActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_main, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Toast.makeText(this, "Go home", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.menu_map:
+                Toast.makeText(this, "bring up map view", Toast.LENGTH_SHORT).show();                
+                break;
+
+            case R.id.menu_checkin:
+                Toast.makeText(this, "Check ih", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.menu_share:
+                Toast.makeText(this, "Tapped share", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
     
 }
